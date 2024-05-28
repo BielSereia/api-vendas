e75a25f529cf603230a6e2c3dc448a5d248c9db9
Gabriel <bielsereia2@hotmail.com>
2024-05-28 15:03:53 -0300

    feat: middleware rate limiter

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,14 @@ import { AppError } from '@shared/errors/AppError';
 import '@shared/typeorm/index';
 import { multerConfig } from '@config/upload';
 import { pagination } from 'typeorm-pagination';
+import { rateLimiter } from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use(pagination);
 
